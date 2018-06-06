fe6c24d44aa43c361e9ca849716a4afb7df910af
Christopher <christopher.klammt@gmail.com>
2018-06-06 11:24:55 +0200

    Major Update!
    - Buttons working (up and down)
    - Contacts working
    - automation working
    - display if currently moving up or down working

--- Rollladen Steuerung.asm
+++ Rollladen Steuerung.asm
@@ -6,10 +6,21 @@ B_DOWN 	EQU EIN.1
 C_IS_UP EQU EIN.3
 C_IS_DOWN EQU EIN.4
 
+; Flags
+FLAGS 	EQU 28h
+F_B_PRESSED EQU FLAGS.0
+F_CLOCK_UP EQU FLAGS.1
+F_CLOCK_DOWN EQU FLAGS.2
+
 ; outputs
 AUS 	EQU P3
 M_UP 	EQU AUS.0
 M_DOWN	EQU AUS.1
+
+; LED Matrix
+VERT_ALWAYS_ON EQU AUS.5
+UPPER_LEDS EQU AUS.6
+LOWER_LEDS EQU AUS.7
 
 
 ; INIT
@@ -29,12 +40,17 @@ mov IE, #10010010b
 mov tmod, #00000010b
 mov r7, #17h
 mov r6, #25h
-mov tl0, #0ffh
-mov th0, #0h
+
+; timer speed
+mov tl0, #14h
+mov th0, #50h
 
 ; clear inputs (buttons, contacts and motor)
 ;mov P0, #0h
-;mov P3, #0h
+mov P3, #0h
+
+; setze dauerhaft 1
+SETB VERT_ALWAYS_ON
 
 ; start timer
 starttimer:
@@ -48,33 +64,62 @@ call zeigen
 anfang:
 mov EIN, P0
 
-mov C, B_UP
+CLR C
+ORL C, /B_UP
+ORL C, F_CLOCK_UP
+
+ANL C, /F_B_PRESSED
 ANL C, /M_DOWN
-ANL C, /C_IS_UP
+ANL C, C_IS_UP
+
 JNC S1
 SETB M_UP
+CLR F_CLOCK_UP
 
 S1:
-mov C, B_DOWN
-ORL C, C_IS_UP
+SETB C
+ANL C, /B_DOWN
+ANL C, /F_B_PRESSED
+
+ORL C, /C_IS_UP
+
+ANL C, M_UP
+
 JNC S2
 CLR M_UP
+SETB F_B_PRESSED
 
 S2:
-mov C, B_DOWN
+CLR C
+ORL C, /B_DOWN
+ORL C, F_CLOCK_DOWN
+
+ANL C, /F_B_PRESSED
 ANL C, /M_UP
-ANL C, /C_IS_DOWN
+ANL C, C_IS_DOWN
+
 JNC S3
 SETB M_DOWN
+CLR F_CLOCK_DOWN
 
 S3:
-mov C, B_UP
-ORL C, C_IS_DOWN 
+SETB C
+ANL C, /B_UP
+ANL C, /F_B_PRESSED
+
+ORL C, /C_IS_DOWN 
+
+ANL C, M_DOWN
+
 JNC S4
 CLR M_DOWN
+SETB F_B_PRESSED
 
 S4:
-mov C, M_DOWN
+CLR C
+ORL C, /B_UP
+ORL C, /B_DOWN
+MOV F_B_PRESSED, C
 
 jnb tr0, da
 ajmp anfang
@@ -92,7 +137,8 @@ ajmp anfang
 ;
 timer:
 inc r1
-cjne r1, #09h, nuranzeige
+; timer repeats
+cjne r1, #05h, nuranzeige
 mov r1, #00h
 call countdown
 ret
@@ -102,6 +148,18 @@ call zeigen
 ret
 
 countdown:
+; Uhrzeit Check -> Hochfahren um 23:45
+cjne r6, #26h, clockDown ; check minuten = 38
+cjne r7, #17h, clockDown ; check stunden = 23
+SETB F_CLOCK_UP
+
+clockDown:
+; Uhrzeit Check -> Runterfahren um 23:48
+cjne r6, #28h, clock ; check minuten = 40
+cjne r7, #17h, clock ; check stunden = 23
+SETB F_CLOCK_DOWN
+
+clock:
 cjne r6, #3bh, minuten
 cjne r7, #17h, stunden
 mov r6, #00h
@@ -119,6 +177,19 @@ call zeigen
 ret
 
 zeigen:
+; update LED Matrix
+CLR UPPER_LEDS
+CLR LOWER_LEDS
+
+jnb M_UP, zeigenLower
+SETB UPPER_LEDS
+
+zeigenLower:
+jnb M_DOWN, zeigenClock
+SETB LOWER_LEDS
+
+zeigenClock:
+; update clock
 mov DPTR, #table
 mov a, R6
 mov b, #0ah
